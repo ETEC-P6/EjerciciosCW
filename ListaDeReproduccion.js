0a02739fcd9b4dfda2be8d8372db1503e3e83839
nichocimm <nichocimm@ciencias.unam.mx>
2025-05-24 13:09:00 -0600

    Correcciones y integracion del toString()

--- ListaDeReproduccion.js
+++ ListaDeReproduccion.js
@@ -4,7 +4,7 @@ export class ListaDeReproduccion{
 
     constructor(arreglo){
         this.lista = null;
-        if(!Array.isArray(array))
+        if(!Array.isArray(arreglo))
             this.lista = [];
         else
             this.lista = structuredClone(arreglo); 
@@ -12,7 +12,7 @@ export class ListaDeReproduccion{
     
 
     pop(indice){
-        let eliminado = this.lista[i];
+        let eliminado = this.lista[indice];
         this.lista.splice(indice,1);
         return eliminado;
     }
@@ -30,21 +30,41 @@ export class ListaDeReproduccion{
     }
 
     forEach(accion){
-        for(i = 0; i < this.lista.length; i++){
+        for(let i = 0; i < this.lista.length; i++){
             accion(this.lista, i);
         }
     }
-
+    toString(){
+        let string = "[";
+        for(let i  = 0; i < this.lista.length-1; i++){
+            string += this.lista[i] + ", ";
+        }
+        string += this.lista[this.getSize()-1] + "]";
+        return string;
+    }
 
 }
 
 function intercambia_shuffle(lista,i){
-    let j = Math.floor(Math.random() * (indice+1))
+    let j = Math.floor(Math.random() * (i+1))
     intercambia(lista,i,j);
 }
 
 function intercambia(lista,a,b){
     let c = lista[a];
     lista[a] = lista[b];
-    lista[b] = lista[c];
+    lista[b] = c;
 }
+
+
+//Ejemplo
+let a = [1,2,3,4,5,6,7,8,9,0];
+let lista = new ListaDeReproduccion(a);
+lista.push(10);
+lista.pop(lista.getSize()-1);
+console.log(lista.toString())
+for(let i = 0; i < lista.getSize(); i++){
+    lista.shuffle();
+    console.log(lista.toString());
+}
+
