61e9e240c67d61399243f76362d1f526767adf27
MariaEmiliaRG <mariaemiliaramirezgomez@comunidad.unam.mx>
2025-05-26 17:26:29 -0600

    modifacación de los ids de la base de datos

---
+++ BaseDeDatos.js
@@ -0,0 +1,42 @@
+const baseDatosJSON = [
+    {
+        id: 1,
+        nombre: "Wake me up",
+        artista: "Avicii",
+        album: "True",
+        link: "69Fb6XozEx8", 
+        genero: "A",
+    },
+    {
+        id: 2,
+        nombre: "Hey brother",
+        artista: "Avicii",
+        album: "True",
+        link: "69Fb6XozEx8", 
+        genero: "A",
+    },
+    {
+        id: 3,
+        nombre: "Alone",
+        artista: "Marshmello",
+        album: "Marshmello",
+        link: "69Fb6XozEx8",
+        genero: "B",
+    },
+    {
+        id: 4,
+        nombre: "Animals",
+        artista: "Martin Garrix",
+        album: "Animals",
+        link: "69Fb6XozEx8",
+        genero: "B",
+    },
+    {
+        id: 5,
+        nombre: "Titanium",
+        artista: "David Guetta",
+        album: "Nothing but the Beat",
+        link: "69Fb6XozEx8",
+        genero: "C",
+    }
+];
